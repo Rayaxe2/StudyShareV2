7fc2161370f37ef89780d3dc6e91f70fcf9fba12
Rayaxe2 <rayaxemailbox@gmail.com>
2019-03-22 03:48:16 +0000

    commit 8

--- src/index.php
+++ src/index.php
@@ -7,29 +7,37 @@ $loginbtn;
 $uploadBtn;
 $registeredUserObj;
 
-if(isset($_SESSION['loggedIn'])){ //|| isset($_SESSION)
-  $loginbtn = '<button class="btn" name="logout">Logout</button>';
+if(isset($_POST['logout'])){
+  session_unset();
+  session_destroy();
+  $loginbtn = '<button class="btn" name="login" id="loginbtn">logout</button>';
+  $uploadBtn = "<a></a>"; 
+  
+  $_POST['logout'] = false;
+  console_log("Loggin button selected");
+}
+
+if(isset($_SESSION['loggedIn'])){ 
+  $loginbtn = '<button class="btn" name="logout" id="logoutbtn">Logout</button>';
   $registeredUserObj = new RegisteredUser;
   $registeredUserObj->setUserName($_SESSION['username']);
   $_SESSION['registeredUser'] = $registeredUserObj;
   $uploadBtn = "<a href=addpost.html>Upload Notes</a>"; 
   console_log("Logged out button selected");
+
+include_once 'index.html';
+ echo "<script>
+    document.getElementById('loginbtn').remove;
+    document.getElementById('LoginButton').innerHTML='$loginbtn';
+    document.getElementById('Uploadbtn').innerHTML='$uploadBtn';
+  </script>";
 }
 else{
-  $loginbtn = '<button class="btn" name="login">Login/SignUp</button>'; //if not logged in
+  $loginbtn = '<button class="btn" name="login" id="loginbtn">logout</button>'; //if not logged in
   $uploadBtn = "<a></a>"; 
 }
 
-if(isset($_POST['logout'])){
-  session_start();
-  session_unset();
-  session_destroy();
-  console_log("Logged out");
-  $loginbtn = '<button class="btn" name="login">Login/SignUp</button>';
-  $uploadBtn = "<a></a>"; 
-}
-
-if(isset($_POST['login'])){
+if(isset($_POST['login']) && (isset($_SESSION['loggedIn'])==false)){
   header("location: login.php");
 }
 
@@ -40,10 +48,13 @@ function console_log( $data ){
 }
   
 include_once 'index.html';
- echo "<script>
+/*
+if(isset($_SESSION['loggedIn'])){
+  echo "<script>
     document.getElementById('loginbtn').remove;
     document.getElementById('LoginButton').innerHTML='$loginbtn';
     document.getElementById('Uploadbtn').innerHTML='$uploadBtn';
   </script>";
-
+}
+*/
 ?>
